5142180b0f4496a9feb57d2201c180e405b38dc4
Pau Garcia <pauligarcia_05@hotmail.com>
2022-02-17 12:46:41 -0300

    Color box added css & js, README modified

--- color_app/src/App.js
+++ color_app/src/App.js
@@ -2,11 +2,12 @@ import React, {Component} from "react";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 
+
 class App extends Component {
   render() {
     return (
       <div>
-        <Palette {...seedColors} />
+        <Palette {...seedColors[4]} />
       </div>
     );
   }

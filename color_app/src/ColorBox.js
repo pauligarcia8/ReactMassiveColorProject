8da6c42135ecc2558b24182f48842cd56635a89e
Pau Garcia <pauligarcia_05@hotmail.com>
2022-02-24 11:32:16 -0300

    finish refactor of colorBox

--- color_app/src/ColorBox.js
+++ color_app/src/ColorBox.js
@@ -9,7 +9,7 @@ import './ColorBox.css';
 const styles = {
     ColorBox: {
         width: "20%",
-        height: props => (props.showingFullPallete ? "25%" : "50%"),
+        height: props => (props.showingFullPalette ? "25%" : "50%"),
         margin: "0 auto",
         display: "inline-block",
         position: "relative",
@@ -60,6 +60,70 @@ const styles = {
         textDecoration: "none",
         opacity: 0
       },
+      boxContent: {
+        position: "absolute",
+        width: "100%",
+        left: "0px",
+        bottom: "0px",
+        padding: "10px",
+        color: "black",
+        letterSpacing: "1px",
+        textTransform: "uppercase",
+        fontSize: "12px"
+      },
+      copyOverlay: {
+        opacity: "0",
+        zIndex: "0",
+        width: "100%",
+        height: "100%",
+        transition: "transform 0.6s ease-in-out",
+        transform: "scale(0.1)"
+      },
+      showOverlay: {
+        opacity: "1",
+        transform: "scale(50)",
+        zIndex: "10",
+        position: "absolute"
+      },
+      copyMessage: {
+        position: "fixed",
+        left: "0",
+        right: "0",
+        top: "0",
+        bottom: "0",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        fontSize: "2.5rem",
+        transform: "scale(0.1)",
+        opacity: "0",
+        color: "white",
+        "& h1": {
+          fontWeight: "400",
+          textShadow: "1px 2px black",
+          background: "rgba(255, 255, 255, 0.2)",
+          width: "100%",
+          textAlign: "center",
+          marginBottom: "0",
+          padding: "1rem",
+          textTransform: "uppercase",
+        //   [sizes.down("xs")]: {
+        //     fontSize: "6rem"
+        //   }
+        },
+        "& p": {
+          fontSize: "2rem",
+          fontWeight: "100"
+        }
+      },
+      showMessage: {
+        opacity: "1",
+        transform: "scale(1)",
+        zIndex: "25",
+        transition: "all 0.4s ease-in-out",
+        transitionDelay: "0.3s"
+      }
 };
 
 class ColorBox extends Component {
@@ -78,25 +142,25 @@ class ColorBox extends Component {
             name, 
             background, 
             moreUrl, 
-            showingFullPallete, 
+            showingFullPalette, 
             classes
         } = this.props;
         const {copied} = this.state;
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
             <div style={{background}} className={classes.ColorBox}>
-            <div style={{background}} className={`copy-overlay ${copied && "show"}`}></div>
-            <div className={`copy-msg ${copied && "show"}`}>
+            <div style={{background}} className={`${classes.copyOverlay} ${copied && classes.showOverlay}`}></div>
+            <div className={`${classes.copyMessage} ${copied && classes.showMessage}`}>
                 <h1>copied!</h1>
                 <p className={classes.copyText}>{this.props.background}</p>
             </div>
-                <div className="copy-container">
-                    <div className="box-content">
+                <div>
+                    <div className={classes.boxContent}>
                         <span className={classes.colorName}>{name}</span>
                     </div>
                     <button className={classes.copyButton}>Copy</button>
                 </div>
-                {showingFullPallete && (
+                {showingFullPalette && (
                     <Link to={moreUrl} onClick={e => e.stopPropagation()}>
                         <span className={classes.seeMore}>MORE</span>
                     </Link>

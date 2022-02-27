db6d53137a002637a8a325d22ec37f4322044c60
Pau Garcia <pauligarcia_05@hotmail.com>
2022-02-27 00:53:12 -0300

    add hooks to NewPaletteForm

--- color_app/src/NewPaletteForm.js
+++ color_app/src/NewPaletteForm.js
@@ -1,13 +1,155 @@
-import React, { Component } from "react";
+// ESTE COMPONENTE SE ACTUALIZO UTILIZANDO HOOKS YA QUE EL DRAWER DE MATERIAL UI CAMBIO MUCHO DESDE QUE LO UTILIZO COLT STEELE CUANDO HIZO EL CURSO.
 
-class NewPaletteForm extends Component {
-    render() {
-        return (
-            <div>
-                <h1>NewPaletteForm</h1>
-            </div>
+// import * as React from 'react';
+import React, { useState } from "react";
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import { ChromePicker } from 'react-color';
+import { Button } from '@mui/material';
 
-        )
-    }
+const drawerWidth = 400;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  }),
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+export default function NewPaletteForm() {
+  
+  const [open2, setOpen2] = useState(true);  
+  const [currentColor, setCurrentColor] = useState('teal'); // Hook creado para manejar el color del color picker y el boton
+  const [colors, setColors] = useState(['purple', '#15764']);
+  //Material UI methods
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const updateNewColor = (newColor) => {
+    console.log(newColor.hex);
+    setCurrentColor(newColor.hex);
+  };
+
+  const addNewColor = () => {
+    setColors([...colors, ...currentColor])
+    console.log(colors)
+  };
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Persistent drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+        <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <Typography variant='h4'>
+            Design Your Palette
+        </Typography>
+        <div>
+            <Button variant='contained' color='secondary'>Create Palette</Button>
+            <Button variant='contained' color='primary'>Random Color</Button>
+        </div>
+          <ChromePicker color={currentColor} onChangeComplete={updateNewColor}/>
+          <Button 
+          variant='contained' 
+          style={{backgroundColor: currentColor}}
+          onClick={addNewColor}
+          >Add Color</Button>
+      </Drawer>
+      <Main open={open}>
+        <DrawerHeader />
+        <ul>
+          {colors.map(color => (
+            <li>{color}</li>
+          ))}
+        </ul>
+      </Main>
+    </Box>
+  );
 }
-export default NewPaletteForm;

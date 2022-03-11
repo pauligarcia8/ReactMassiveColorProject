cb709b7d85dd3010ce1d76f52b9cac43d2675c65
Pau Garcia <pauligarcia_05@hotmail.com>
2022-03-11 09:29:18 -0300

    add page component to do transition over routes

--- color_app/src/App.js
+++ color_app/src/App.js
@@ -9,6 +9,7 @@ import SingleColorPalette from "./SingleColorPalette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 import NewPaletteForm from "./NewPaletteForm";
+import Page from "./Page";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 class App extends Component {
@@ -51,42 +52,53 @@ class App extends Component {
      return (
        <Route render={({location}) => (
          <TransitionGroup>
-         <CSSTransition key={location.key} classNames='fade' timeout={5000}>
+         <CSSTransition 
+         key={location.key} 
+         classNames='page'
+         timeout={500}>
             <Switch location={location}>
             <Route 
             exact 
             path="/palette/new" 
             render={routeProps => (
+              <Page>
               <NewPaletteForm savePalette={this.savePalette} palettes={this.state.palettes} {...routeProps} />
+              </Page>
               )}
               />
             <Route 
               path='/palette/:paletteId/:colorId' 
               render={routeProps => ( 
+                <Page>
                 <SingleColorPalette
                 colorId={routeProps.match.params.colorId}
                 palette={generatePalette(
                 this.findPalette(routeProps.match.params.paletteId)
                 )} 
                 />
+                </Page>
               )} 
             />     
             <Route 
             exact 
             path="/" 
             render={routeProps => (
+              <Page>
               <PaletteList palettes={this.state.palettes} deletePalette={this.deletePalette} {...routeProps} />
+              </Page>
               )} //routeProps se importa para acceder a metodos como history para poder usarlo dentro de paletteList
-            />
+            />           
             <Route 
               exact 
               path="/palette/:id" 
               render={routeProps => ( 
+                <Page>
                 <Palette 
                 palette={generatePalette(
                   this.findPalette(routeProps.match.params.id)
                 )}
                 /> 
+                </Page>
               )}
             />
           </Switch>
